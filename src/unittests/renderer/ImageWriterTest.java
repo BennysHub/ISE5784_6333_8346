f226cfd138e24c500f3c5c9fc620eb5685ad77c5
Benny <biavraha@g.jct.ac.il>
2024-07-14 20:32:48 +0300

    part 5 start

---
+++ src/unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,26 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageWriterTest {
+
+    @Test
+    void getNy() {
+    }
+
+    @Test
+    void testss() {
+
+    }
+
+    @Test
+    void writeToImage() {
+
+    }
+
+    @Test
+    void writePixel() {
+    }
+}
