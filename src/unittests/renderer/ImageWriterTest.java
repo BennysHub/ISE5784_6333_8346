10a368566b91550c45fb1b4aff46860829325a33
Benny <biavraha@g.jct.ac.il>
2024-07-14 21:05:30 +0300

    part 5 mid

--- src/unittests/renderer/ImageWriterTest.java
+++ src/unittests/renderer/ImageWriterTest.java
@@ -1,26 +1,23 @@
 package renderer;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImageWriterTest {
 
     @Test
-    void getNy() {
-    }
+    void gridTest() {
+        ImageWriter imageWriter = new ImageWriter("grid", 800, 500);
+        for (int x = 0; x < 800; x++)
+            for (int y = 0; y < 500; y++)
+                if (x % 50 == 0 || y % 50 == 0) {
+                    imageWriter.writePixel(x, y, new Color(90, 30, 70));
+                } else
+                    imageWriter.writePixel(x, y, new Color(34, 231, 20)); ;
 
-    @Test
-    void testss() {
 
-    }
-
-    @Test
-    void writeToImage() {
-
-    }
-
-    @Test
-    void writePixel() {
+        imageWriter.writeToImage();
     }
 }
